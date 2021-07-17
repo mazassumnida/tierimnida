e1fd2b5391f8f677e5bc488ab22220d9c2d9330a
strawji02 <strawji02@naver.com>
2021-07-17 22:23:45 +0900

    edit button slider toggle

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,14 +1,18 @@
 var enabled = true; //초기 정렬상태
 var myButton = document.getElementById("toggle");
+var check = $("input[type='checkbox']");
+
 chrome.storage.local.get("enabled", (data) => {
   enabled = data.enabled;
-  myButton.textContent = enabled ? "Disable" : "Sort";
+  if (enabled) {
+    $("input:checkbox").prop("checked", true);
+  }
 });
-myButton.onclick = () => {
+
+check.click(function () {
   enabled = !enabled;
-  myButton.textContent = enabled ? "Disable" : "Sort";
   chrome.storage.local.set({ enabled: enabled });
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.tabs.sendMessage(tabs[0].id, { action: "TOGGLE" });
   });
-};
+});
