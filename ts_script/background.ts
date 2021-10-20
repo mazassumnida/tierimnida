8d3cd5b8202869392ecd6523569acd58b5c4d932
co0ki3 <ehrrhgus0324@gmail.com>
2021-10-20 20:27:54 +0900

    end

--- ts_script/background.ts
+++ ts_script/background.ts
@@ -1,9 +1,5 @@
-chrome.runtime.onMessage.addListener(function (
-  message,
-  sender,
-  sendResponse,
-) {
+chrome.runtime.onMessage.addListener(function (message, sender) {
   if (typeof message === 'object' && message.type === 'showPageAction') {
-    chrome.pageAction.show((<{ tab: { id: number }}>sender).tab.id);
+    chrome.pageAction.show((<{ tab: { id: number } }>sender).tab.id);
   }
 });
--- ts_script/popup.ts
+++ ts_script/popup.ts
@@ -1,11 +1,11 @@
 var enabled = true; //초기 정렬상태
-var myButton = document.getElementById("toggle");
+var myButton = document.getElementById('toggle');
 var check = $("input[type='checkbox']");
 
-chrome.storage.local.get("enabled", (data) => {
+chrome.storage.local.get('enabled', (data) => {
   enabled = data.enabled;
   if (enabled) {
-    $("input:checkbox").prop("checked", true);
+    $('input:checkbox').prop('checked', true);
   }
 });
 
@@ -13,6 +13,6 @@ check.click(function () {
   enabled = !enabled;
   chrome.storage.local.set({ enabled: enabled });
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, { action: "TOGGLE" });
+    chrome.tabs.sendMessage(tabs[0]?.id ?? 0, { action: 'TOGGLE' });
   });
 });
